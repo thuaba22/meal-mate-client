ec6aea794ac23f53f40eb2afb62d1ab06a09af7e
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-11-27 16:19:08 +0600

    updated the review section with backend

--- src/pages/MealDetails/MealDetails.jsx
+++ src/pages/MealDetails/MealDetails.jsx
@@ -4,16 +4,20 @@ import Rating from "../../components/MealsByCategory/Rating";
 import { FaThumbsUp } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
-
+import { AwesomeButton } from "react-awesome-button";
+import "react-awesome-button/dist/styles.css";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
-import { useState } from "react";
+import { useContext, useState } from "react";
 import PageTitle from "../../components/shared/PageTitle/PageTitle";
+import { AuthContext } from "../../providers/AuthProvider";
+import { toast } from "react-toastify";
 
 const MealDetails = () => {
   // Fetch meal details using useLoaderData hook
   const meals = useLoaderData();
+  const auth = useContext(AuthContext);
 
   // State variable to track the number of likes
   const [likes, setLikes] = useState(meals.likes);
@@ -21,6 +25,8 @@ const MealDetails = () => {
   // State variable to track whether the user has liked the meal
   const [liked, setLiked] = useState(false);
 
+  const [reviewText, setReviewText] = useState("");
+  const [reviews, setReviews] = useState(meals.reviews);
   // Function to handle the like button click
   const handleLike = async () => {
     try {
@@ -49,6 +55,37 @@ const MealDetails = () => {
     }
   };
 
+  const handleSubmitReview = async () => {
+    try {
+      const response = await fetch("http://localhost:5000/meals/review", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          mealId: meals._id,
+          user: auth.user.displayName,
+          comment: reviewText,
+        }),
+      });
+
+      if (response.ok) {
+        // Update the reviews state with the new review
+        setReviews([
+          ...reviews,
+          { user: auth.user.displayName, comment: reviewText },
+        ]);
+        toast("Thank You For Your Valuable Reviews!");
+        // Clear the review text
+        setReviewText("");
+      } else {
+        console.error("Failed to submit review.");
+      }
+    } catch (error) {
+      console.error("Failed to submit review", error);
+    }
+  };
+
   return (
     <div>
       <PageTitle title="MealMate | Meal Details"></PageTitle>
@@ -72,6 +109,39 @@ const MealDetails = () => {
           <button className="btn btn-outline bg-[#45D62D] text-white">
             Request A Meal
           </button>
+          {/* Open the modal using document.getElementById('ID').showModal() method */}
+          <button
+            className="btn btn-outline bg-[#45D62D] text-white"
+            onClick={() => document.getElementById("my_modal_5").showModal()}
+          >
+            Give Review
+          </button>
+          <dialog
+            id="my_modal_5"
+            className="modal modal-bottom sm:modal-middle"
+          >
+            <div className="modal-box space-y-3">
+              <h3 className="font-bold text-[#216D30] text-lg">
+                Please Feel Free To Share
+              </h3>
+              <textarea
+                className="textarea w-[90%] textarea-success"
+                placeholder="Type Here.."
+                value={reviewText}
+                onChange={(e) => setReviewText(e.target.value)}
+              ></textarea>
+              <AwesomeButton type="primary" onPress={handleSubmitReview}>
+                Submit
+              </AwesomeButton>
+
+              <div className="modal-action">
+                <form method="dialog">
+                  {/* if there is a button in form, it will close the modal */}
+                  <button className="btn">Close</button>
+                </form>
+              </div>
+            </div>
+          </dialog>
 
           {/* Like button with conditional style and disabled attribute based on 'liked' state */}
           <button
@@ -126,22 +196,26 @@ const MealDetails = () => {
           {/* Reviews using Swiper */}
           <div className="mt-4 text-[#68a26c] text-md font-semibold">
             <p>Reviews: </p>
-            <Swiper
-              navigation={true}
-              modules={[Navigation]}
-              className="mySwiper"
-            >
-              {meals.reviews.map((review, index) => (
-                <SwiperSlide key={index}>
-                  <div className="flex flex-col items-center mx-24 my-16">
-                    <p className="py-3 text-[#68a26c]">{review.user}</p>
-                    <h3 className="text-2xl text-[#216D30]">
-                      {review.comment}
-                    </h3>
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
+            {reviews.length > 0 ? (
+              <Swiper
+                navigation={true}
+                modules={[Navigation]}
+                className="mySwiper"
+              >
+                {reviews.map((review, index) => (
+                  <SwiperSlide key={index}>
+                    <div className="flex flex-col items-center mx-24 my-16">
+                      <p className="py-3 text-[#68a26c]">{review.user}</p>
+                      <h3 className="text-2xl text-[#216D30]">
+                        {review.comment}
+                      </h3>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            ) : (
+              <p>No reviews yet.</p>
+            )}
           </div>
         </div>
       </div>
