c9045ad9a736ae4a69f78fb61bbc22f263e25197
thuaba22 <mim.islam.tabassuma@gmail.com>
2023-12-01 12:26:58 +0600

    added pagination to the upcoming meals page

--- src/pages/UpcomingMeals/UpcomingMeals.jsx
+++ src/pages/UpcomingMeals/UpcomingMeals.jsx
@@ -13,6 +13,7 @@ const UpcomingMeals = () => {
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState(null);
   const [priceRange, setPriceRange] = useState({ min: "", max: "" });
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     setTimeout(() => {
@@ -20,7 +21,7 @@ const UpcomingMeals = () => {
         .then((response) => response.json())
         .then((data) => {
           setUpcomingMeals(data);
-          setFilteredUpcomingMeals(data); // Initially, set filteredUpcomingMeals to all upcoming meals
+          setFilteredUpcomingMeals(data);
           setLoading(false);
 
           // Extract unique categories from upcoming meals
@@ -57,8 +58,19 @@ const UpcomingMeals = () => {
     }
   }, [searchQuery, selectedCategory, upcomingMeals]);
 
+  useEffect(() => {
+    // Filter upcoming meals based on the price range
+    const filtered = upcomingMeals.filter(
+      (meal) =>
+        (!priceRange.min || meal.price >= parseFloat(priceRange.min)) &&
+        (!priceRange.max || meal.price <= parseFloat(priceRange.max))
+    );
+    setFilteredUpcomingMeals(filtered);
+  }, [priceRange, upcomingMeals]);
+
   const handleSearch = (e) => {
     setSearchQuery(e.target.value);
+    setCurrentPage(1);
   };
 
   const handlePriceFilter = () => {
@@ -69,6 +81,7 @@ const UpcomingMeals = () => {
         (!priceRange.max || meal.price <= parseFloat(priceRange.max))
     );
     setFilteredUpcomingMeals(filtered);
+    setCurrentPage(1);
   };
 
   const handleLike = (mealId) => {
@@ -89,11 +102,23 @@ const UpcomingMeals = () => {
       });
   };
 
+  const indexOfLastItem = currentPage * 6; // You can adjust this as needed
+  const indexOfFirstItem = indexOfLastItem - 6;
+  const currentItems = filteredUpcomingMeals.slice(
+    indexOfFirstItem,
+    indexOfLastItem
+  );
+
+  const pageNumbers = [];
+  for (let i = 1; i <= Math.ceil(filteredUpcomingMeals.length / 6); i++) {
+    pageNumbers.push(i);
+  }
+
   return (
     <div>
-      <PageTitle title="MealMate | Upcoming Meals"></PageTitle>
+      <PageTitle title="MealMate | Upcoming Meals" />
 
-      <Navbar></Navbar>
+      <Navbar />
       <div className="mt-20 w-[80%] mx-auto mb-20">
         <div className="overflow-x-auto">
           <div className="flex ml-2 gap-2 items-center">
@@ -161,40 +186,58 @@ const UpcomingMeals = () => {
               visible={true}
             />
           ) : (
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
-              {filteredUpcomingMeals.map((meal) => (
-                <div key={meal._id} className="bg-[#ECFCE8] p-4 rounded shadow">
-                  <img
-                    className="w-full h-48 object-cover mb-4 rounded"
-                    src={meal.image}
-                    alt={meal.title}
-                  />
-                  <h2 className="text-xl font-semibold mb-2">{meal.title}</h2>
-                  <p className="text-gray-600 mb-2">{meal.meal_category}</p>
-                  <p className="text-gray-600 mb-4">
-                    Distributor: {meal.admin_name}
-                  </p>
-                  <p className="text-gray-600 mb-4">
-                    Post Time: {meal.post_time}
-                  </p>
-                  <p className="text-green-600 font-semibold mb-2">
-                    {meal.price}$
-                  </p>
-                  <div className="flex items-center">
-                    <button
-                      onClick={() => handleLike(meal._id)}
-                      className="btn bg-[#45D62D] text-white hover:bg-[#45D62D] mr-2"
-                    >
-                      Like ({meal.likes})
-                    </button>
+            <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-4">
+                {currentItems.map((meal) => (
+                  <div
+                    key={meal._id}
+                    className="bg-[#ECFCE8] p-4 rounded shadow"
+                  >
+                    <img
+                      className="w-full h-48 object-cover mb-4 rounded"
+                      src={meal.image}
+                      alt={meal.title}
+                    />
+                    <h2 className="text-xl font-semibold mb-2">{meal.title}</h2>
+                    <p className="text-gray-600 mb-2">{meal.meal_category}</p>
+                    <p className="text-gray-600 mb-4">
+                      Distributor: {meal.admin_name}
+                    </p>
+                    <p className="text-gray-600 mb-4">
+                      Post Time: {meal.post_time}
+                    </p>
+                    <p className="text-green-600 font-semibold mb-2">
+                      {meal.price}$
+                    </p>
+                    <div className="flex items-center">
+                      <button
+                        onClick={() => handleLike(meal._id)}
+                        className="btn bg-[#45D62D] text-white hover:bg-[#45D62D] mr-2"
+                      >
+                        Like ({meal.likes})
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
+              <div className="mt-4 flex justify-center space-x-2">
+                {pageNumbers.map((number) => (
+                  <button
+                    key={number}
+                    onClick={() => setCurrentPage(number)}
+                    className={`btn ${
+                      number === currentPage ? "bg-[#45D62D] text-white" : ""
+                    }`}
+                  >
+                    {number}
+                  </button>
+                ))}
+              </div>
             </div>
           )}
         </div>
       </div>
-      <Footer></Footer>
+      <Footer />
     </div>
   );
 };
